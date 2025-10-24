9cdcb4198d37ec17741b37b49cd1c877f6339c69
Inés <ines.ramos@campusdual.com>
2025-10-24 17:37:25 +0200

    solved exercise 12

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,7 +2,30 @@ package com.campusdual.classroom;
 
 public class Exercise {
     public static void main(String[] args) {
+        Car car1 = new Car("Toyota", "Corolla", "Gasolina");
+
+        // Encender el coche
+        car1.start();
+
+        // Acelerar y mostrar velocidad
+        car1.accelerate();
+        System.out.println("Velocidad actual: " + car1.speedometer + " km/h");
+
+        // Girar el volante
+        car1.turnAngleOfWheels(30);
+        System.out.println(car1.showSteeringWheelDetail());
+
+        // Activar marcha atrás
+        car1.setReverse(true);
+        System.out.println("Marcha atrás activada? " + car1.isReverse());
+
+        // Frenar y apagar
+        car1.brake();
+        car1.stop();
+
+        // Mostrar todos los detalles del coche
+        car1.showDetails();
+    }
 
     }
 
-}
